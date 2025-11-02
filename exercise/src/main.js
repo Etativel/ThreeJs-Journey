683c5ce6e0c3146f972e1e6d127917b629c78e9a
Etativel <farhanmaulana4611@gmail.com>
2025-11-02 19:40:12 +0700

    feat: rotating cube

--- exercise/src/main.js
+++ exercise/src/main.js
@@ -1,24 +1,72 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "three";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas = document.querySelector(".webgl");
+const btnZoomIn = document.querySelector(".btn-zoomin");
+const btnZoomOut = document.querySelector(".btn-zoomout");
 
-setupCounter(document.querySelector('#counter'))
+let cameraPosition = 3;
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const size = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  45,
+  size.width / size.height,
+  1,
+  1000
+);
+camera.position.z = cameraPosition;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(size.width, size.height);
+
+function animate(time) {
+  // time is in milliseconds
+  const seconds = time * 0.001;
+
+  // Make the cube rotate slowly
+  mesh.rotation.x = seconds;
+  mesh.rotation.y = seconds;
+
+  // Make it move up and down
+  mesh.position.y = Math.sin(seconds) * 0.5;
+
+  // Render the updated frame
+  renderer.render(scene, camera);
+
+  // Request the next frame
+  requestAnimationFrame(animate);
+}
+
+// Initialize animation
+requestAnimationFrame(animate);
+
+btnZoomOut.addEventListener("click", () => {
+  cameraPosition++;
+  camera.position.z = cameraPosition;
+});
+
+btnZoomIn.addEventListener("click", () => {
+  cameraPosition--;
+  camera.position.z = cameraPosition;
+});
